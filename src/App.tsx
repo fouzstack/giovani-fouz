ad7e1eb143e8d996d9e7b5c7d89745ab6a8e8139
fouzstack <fouzlaura@gmail.com>
2025-12-22 23:17:23 -0500

    new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import HybridEcosystemLanding from './HybridEcosystemWeb';
+import HybridEcosystemLanding from './HybridEcosystemWeb2';
 //import LicenseGeneratorOffline from './LicenseGeneratorOffline';
 //import LicenseVerifier from './VerifierDarkPremium';
+//import CosmicArchitectureLanding from './CosmicArchitectureLanding';
+
 
 
 function App() {
